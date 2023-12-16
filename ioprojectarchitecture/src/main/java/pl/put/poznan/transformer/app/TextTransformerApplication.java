73ddb3d93dac0683f990b8abc9992c532db5ef8d
DeporS <120829904+DeporS@users.noreply.github.com>
2023-12-16 20:36:30 +0100

    change main

--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -2,12 +2,21 @@ package pl.put.poznan.transformer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.put.poznan.transformer.logic.JsonComparator;
 
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class TextTransformerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TextTransformerApplication.class, args);
+
+        // compare jsons from path
+        CompareJsons("src/main/resources/changed.json", "src/main/resources/full.json");
+    }
+
+    private static void CompareJsons(String jsonFilePath1, String jsonFilePath2){
+        JsonComparator.compareAndDisplayDifferences(jsonFilePath1, jsonFilePath2);
     }
 }
