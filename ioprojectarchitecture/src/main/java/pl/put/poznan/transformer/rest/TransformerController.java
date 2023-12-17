37564ff3cb3a98a8d0b680154ff24548c2a3fccc
alicjalis <57289626+alicjalis@users.noreply.github.com>
2023-12-17 22:33:39 +0100

    added all functions to controller, added tests

--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/rest/TransformerController.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/rest/TransformerController.java
@@ -4,7 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.Filter;
 import pl.put.poznan.transformer.logic.FilterOnly;
+import pl.put.poznan.transformer.logic.JSON;
 import pl.put.poznan.transformer.logic.JsonComparator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 
 @RestController
 @RequestMapping("/transformer")
@@ -13,6 +20,9 @@ public class TransformerController {
     private final JsonComparator jsonComparator;
     private final Filter filter;
     private final FilterOnly filterOnly;
+
+    private final Map<String, JSON> jsonRepository = new HashMap<>();
+
 
     public TransformerController() {
         this.jsonComparator = new JsonComparator();
@@ -35,4 +45,29 @@ public class TransformerController {
     public String filterOnly(@RequestBody String text, @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) throws JsonProcessingException {
         return filterOnly.decorate(text, transforms);
     }
+
+    @PostMapping("/json")
+    public String createJSON(@RequestBody String jsonString) {
+        JSON json = new JSON(jsonString);
+        String id = UUID.randomUUID().toString();
+        jsonRepository.put(id, json);
+        return id;
+    }
+
+    @GetMapping("/json/{id}")
+    public String getJSON(@PathVariable String id) {
+        JSON json = jsonRepository.get(id);
+        if (json == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "JSON with id " + id + " not found");
+        }
+        try {
+            return json.getString();
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error processing JSON", e);
+        }
+    }
+
+
+
+
 }
