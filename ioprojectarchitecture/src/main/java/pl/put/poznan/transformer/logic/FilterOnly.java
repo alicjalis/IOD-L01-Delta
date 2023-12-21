556a08e0ef85d55d9310519ee1562f72767a8320
alicjalis <57289626+alicjalis@users.noreply.github.com>
2023-12-21 01:20:28 +0100

    added part of FilterOnly documentation

--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/FilterOnly.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/FilterOnly.java
@@ -7,18 +7,41 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.*;
 
+/**
+ * Klasa FilterOnly służy do filtrowania pól JSON.
+ * Zawiera metody do filtrowania tekstu.
+ */
 public class FilterOnly extends JsonDecorator {
     private final String[] filterParameters;
-    public FilterOnly(JSONTransformer json, String[] filterParameter)  {
+
+    /**
+     * Konstruktor klasy FilterOnly.
+     *
+     * @param json           Obiekt klasy JSONTransformer.
+     * @param filterParameter Tablica parametrów filtrowania.
+     */
+    public FilterOnly(JSONTransformer json, String[] filterParameter) {
         super(json);
         filterParameters = filterParameter;
     }
 
+    /**
+     * Metoda DO UZUPELNIENIA
+     *
+     * @param text
+     * @return Zwraca zmieniony tekst
+     */
     public String decorate(String text) {
         return filterOutString(super.decorate(text),filterParameters);
     }
 
-
+    /**
+     * Metoda filtrująca tekst.
+     *
+     * @param text Tekst do filtrowania.
+     * @param filterParameter Parametry filtrowania.
+     * @return Zwraca przefiltrowany tekst.
+     */
     private String filterOutString(String text, String[] filterParameter) {
         JSON jsNode = new JSON(text);
 
@@ -29,7 +52,6 @@ public class FilterOnly extends JsonDecorator {
             ((ObjectNode) toBeRemovedField.get(i)).remove(toBeRemovedFieldName.get(i));
         }
 
-
         try {
             return jsNode.getString();
         } catch (JsonProcessingException e) {
@@ -37,7 +59,16 @@ public class FilterOnly extends JsonDecorator {
         }
     }
 
-    private int filterNodeRecursive(JsonNode jsonNode, String[] filter, List<JsonNode>  forRemoval, ArrayList<String> forRemovalName) {
+    /**
+     * Metoda rekurencyjnie filtrująca plik JSON.
+     *
+     * @param jsonNode Węzeł JSON do filtrowania.
+     * @param filter  Parametry filtrowania.
+     * @param forRemoval Węzły do usunięcia.
+     * @param forRemovalName Nazwy węzłów do usunięcia.
+     * @return Zwraca liczbę węzłów, które spełniają warunki filtrowania.
+     */
+    private int filterNodeRecursive(JsonNode jsonNode, String[] filter, List<JsonNode> forRemoval, ArrayList<String> forRemovalName) {
         int numberOfFilterNodesAbove = 0;
         int numberOfFilterNodesCurrent = 0;
         if (jsonNode.isObject()) {
@@ -46,10 +77,9 @@ public class FilterOnly extends JsonDecorator {
                 Map.Entry<String, JsonNode> field = fields.next();
                 numberOfFilterNodesAbove = filterNodeRecursive((JsonNode) field.getValue(), filter,forRemoval,forRemovalName);
 
-
                 if(Arrays.asList(filter).contains(field.getKey())) {
                     numberOfFilterNodesCurrent += 1;
-                 } else if(numberOfFilterNodesAbove == 0) {
+                } else if(numberOfFilterNodesAbove == 0) {
                     forRemoval.add(jsonNode);
                     forRemovalName.add(field.getKey());
                 }
@@ -64,6 +94,4 @@ public class FilterOnly extends JsonDecorator {
         }
         return numberOfFilterNodesCurrent + numberOfFilterNodesAbove;
     }
-
-
 }
