0f7aa68adfc37b725a9771b7097e561ee59215ae
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-01-22 00:03:13 +0100

    added documentation comments to JSON class

--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/JSON.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/JSON.java
@@ -6,11 +6,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
+/**
+ * Klasa JSON służy do przetwarzania ciągów JSON w formacie JsonNode.
+ * Wykorzystuje bibliotekę Jackson do odczytywania ciągów JSON i konwersji ich na obiekty JsonNode
+ * które mogą być następnie manipulowane i przekształcane z powrotem na ciągi JSON
+ */
 public class JSON {
+    /**
+     * Prywatna zmienna typu JsonNode przechowująca obecnie przetwarzany obiekt JSON.
+     */
     private JsonNode node ;
 
+    /**
+     * Konstruktor klasy JSON. Przyjmuje ciąg JSON jako argument i próbuje go przekształcić na obiekt JsonNode.
+     * W przypadku błędu podczas przetwarzania ciągu JSON, konstruktor rzuca wyjątek RuntimeException.
+     *
+     * @param jsonString Ciąg JSON do przetworzenia.
+     */
     public JSON(String jsonString) {
-        ObjectMapper  objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
         try {
             node = objectMapper.readValue(jsonString, JsonNode.class);
         } catch (IOException e) {
@@ -18,10 +32,22 @@ public class JSON {
         }
     }
 
+    /**
+     * Metoda zwracająca obecny obiekt JsonNode.
+     *
+     * @return obiekt JsonNode.
+     */
     public JsonNode get() {return node;}
 
+    /**
+     * Metoda konwertująca obecny obiekt JsonNode z powrotem na ciąg JSON.
+     * W przypadku błędu podczas konwersji, metoda rzuca wyjątek JsonProcessingException.
+     *
+     * @return Ciąg JSON reprezentujący obecny obiekt JsonNode.
+     * @throws JsonProcessingException Jeżeli wystąpi błąd podczas konwersji obiektu JsonNode na ciąg JSON.
+     */
     public String getString() throws JsonProcessingException {
-        ObjectMapper  objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(node);
     }
 }
