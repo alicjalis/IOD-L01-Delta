b8b63a55437d7a32984d95858106ca50677dc464
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-01-22 00:14:36 +0100

    added documentation comments to JSON, JsonDecorator, JsonDecoratorBuilder(not fully) class

--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/JsonDecorator.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/JsonDecorator.java
@@ -1,12 +1,30 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Klasa JsonDecorator implementuje interfejs JSONTransformer.
+ * Służy do dekorowania obiektów implementujących ten sam interfejs, dodając do nich dodatkowe funkcje.
+ */
 public class JsonDecorator implements JSONTransformer {
+    /**
+     * Zmienna chroniona typu JSONTransformer przechowująca obecnie dekorowany obiekt.
+     */
     protected JSONTransformer transformer;
 
+    /**
+     * Konstruktor klasy JsonDecorator. Przyjmuje obiekt implementujący interfejs JSONTransformer jako argument.
+     *
+     * @param t Obiekt do dekorowania.
+     */
     public JsonDecorator(JSONTransformer t) {
         transformer = t;
     }
 
+    /**
+     * Metoda dekorująca. Wywołuje metodę dekorującą na obecnym obiekcie.
+     *
+     * @param JsonString Ciąg JSON do dekorowania.
+     * @return Zwraca dekorowany ciąg JSON.
+     */
     @Override
     public String decorate(String JsonString) {
         return transformer.decorate(JsonString);
--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/JsonDecoratorBuilder.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/JsonDecoratorBuilder.java
@@ -2,8 +2,21 @@ package pl.put.poznan.transformer.logic;
 
 
 
+/**
+ * Klasa JsonDecoratorBuilder jest klasą do tworzenia obiektów implementujących interfejs JSONTransformer.
+ * Buduje obiekt, dodając do niego różne dekoratory w zależności od podanych parametrów.
+ */
 public class JsonDecoratorBuilder {
 
+    /**
+     * Metoda tworząca i zwracająca obiekt implementujący interfejs JSONTransformer.
+     * Na podstawie podanych parametrów dodaje do obiektu różne dekoratory.
+     *
+     * @param format Format, w którym ma być zwrócony ciąg JSON. Może to być "minify" lub "prettify".
+     * @param filterParameter Tablica zawierająca parametry, które mają być filtrowane.
+     * @param filterOnlyParameter Tablica zawierająca parametry, które mają być filtrowane NIE WIEM CO Z NIMI
+     * @return Zwraca obiekt implementujący interfejs JSONTransformer.
+     */
     public JSONTransformer getDecorator(String format, String[] filterParameter,String[] filterOnlyParameter) {
         JSONTransformer transformer = new JsonDecorator( new BasicJsonTransformer());
 
@@ -24,3 +37,4 @@ public class JsonDecoratorBuilder {
         return transformer;
     }
 }
+
